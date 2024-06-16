5c26bf6a99f8c4eef8d8c1421c74a963af51711e
tirex300 <thetirex29@gmail.com>
2024-06-16 23:22:02 +0300

    Test commit. User, Topic and Quiz were added. Methods will be in next commit

---
+++ main.py
@@ -0,0 +1,97 @@
+from flask import request, jsonify
+from config import app, db
+from models import User, Topic, Quiz
+
+
+@app.route("/")
+def main_page():
+    return "<h1>Welcome to the Quizee backend!</h1>"
+
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    users = User.query.all()
+    json_users = list(map(lambda x: x.to_json(), users))
+
+    return jsonify({"users": json_users}), 200
+
+
+@app.route("/create_user", methods=["POST"])
+def create_user():
+    email = request.json.get("email")
+    username = request.json.get("username")
+    password = request.json.get("password")
+
+    if not username or not email:
+        return jsonify({"message": "You must inclide username and email."}), 400,
+
+    new_user = User(username=username, password=password, email=email)
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message": "User created."}), 201
+
+
+@app.route("/topics", methods=["GET"])
+def get_topics():
+    topics = Topic.query.all()
+    json_topics = list(map(lambda x: x.to_json(), topics))
+
+    return jsonify({"topics": json_topics}), 200
+
+
+@app.route("/create_topic", methods=["POST"])
+def create_topic():
+    naming = request.json.get("naming")
+
+    if not naming:
+        return jsonify({"message": "You must inclide naming."}), 400,
+
+    new_topic = Topic(naming=naming)
+    try:
+        db.session.add(new_topic)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message": "Topic created."}), 201
+
+
+@app.route("/quizzes", methods=["GET"])
+def get_quizzes():
+    quizzes = Quiz.query.all()
+    json_quizzes = list(map(lambda x: x.to_json(), quizzes))
+
+    return jsonify({"topics": json_quizzes}), 200
+
+
+@app.route("/create_quiz", methods=["POST"])
+def create_quiz():
+    naming = request.json.get("naming")
+
+    if not naming:
+        return jsonify({"message": "You must inclide naming."}), 400,
+
+    new_quiz = Quiz(naming=naming)
+    try:
+        db.session.add(new_quiz)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message": "Quiz created."}), 201
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        # some_topic = Topic(naming="Narcotics")
+        # quiz1 = Quiz(naming="Woman with a headache", topic=some_topic)
+        # print(quiz1.topic)
+        # db.session.add_all([some_topic, quiz1])
+        # db.session.commit()
+
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,51 @@
+from config import db
+from datetime import datetime, UTC
+# User
+# Topic -> Quiz
+# Quiz
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(50), nullable=False)  # hash of password
+    creation_date = db.Column(db.Date, default=datetime.now(UTC))
+    # Statistics
+    
+    def to_json(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "username": self.username,
+            "password": self.password,
+            "creationDate": self.creation_date
+        }
+    
+    def __repr__(self) -> str:
+        return f"<User: {self.username} with email: {self.email}>"
+
+
+class Topic(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    naming = db.Column(db.String(80), nullable=False, unique=True)
+    quizzes = db.relationship("Quiz", backref="topic")
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "naming": self.naming
+        }
+
+
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    naming = db.Column(db.String(80), nullable=False, unique=True)
+    topic_id = db.Column(db.Integer, db.ForeignKey("topic.id"), nullable=True)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "naming": self.naming
+        }
+
